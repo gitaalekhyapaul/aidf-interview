6c7c834a9423e344fc8050be225d25cff593f08a
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2025-08-15 13:56:24 +0800

    feat: server boilerplate SKIP_CI
    Signed-off-by: Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+import Express, { Request, Response } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
+const app = Express();
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT || 8080}`
+  );
+});
